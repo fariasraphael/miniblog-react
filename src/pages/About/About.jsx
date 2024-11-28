046c0b59234b3a79b5f6541c08393258e4a0c00d
raphael farias <farias.raphael@gmail.com>
2024-11-27 21:36:03 -0300

    start abutpage

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
 import styles from "./About.module.css";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h2>Sobre o Mini </h2>
+      <span>BLOG</span>
+      <p>
+        Projeto consiste em um blog feito com react no front-end e Firebase no
+        back-end.
+      </p>
+      <Link to="/post/create" className="btn">
+        Criar post
+      </Link>
     </div>
   );
 };
