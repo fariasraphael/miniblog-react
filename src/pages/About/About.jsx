92f538c69ecee3728dddd164423cb36739bd60fa
raphael farias <farias.raphael@gmail.com>
2024-11-27 22:31:11 -0300

    add css styles.about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 import styles from "./About.module.css";
 const About = () => {
   return (
-    <div>
-      <h2>Sobre o Mini </h2>
-      <span>BLOG</span>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
       <p>
         Projeto consiste em um blog feito com react no front-end e Firebase no
         back-end.
